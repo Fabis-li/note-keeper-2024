6030fb375e84fa30e980402b1e7da64231a7995e
Fabis-li <fabiano_slima@hotmail.com>
2024-10-07 19:55:51 -0300

    criado cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { ShellComponent } from "./core/components/shell/shell.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ShellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
